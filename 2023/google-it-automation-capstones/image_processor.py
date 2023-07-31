530a821dfa2967d03db11adfa211e453d92d7777
Ben Porter <102399623+nuiben@users.noreply.github.com>
2023-07-30 22:38:33 -0400

    Added comments to provide summary overview of this script's functionality.

--- 2023/google-it-automation-capstones/image_processor.py
+++ 2023/google-it-automation-capstones/image_processor.py
@@ -11,10 +11,14 @@ import os
 img_path = './images/'
 save_to_path = '/opt/icons/'
 
+# Takes a string paramater, opens the matching file name using PIL.Image,
+# rotates the image 90 degrees clockwise, resizing it to 128x128, and saves as a '.JPEG' to 'opt/icons/'
 def flip_and_save(image_name):
     im = Image.open(img_path + image_name)
     im.rotate(-90).resize((128,128)).convert('RGB').save(save_to_path + image_name, "JPEG")
 
+# Using the os module, the './images' directory is parsed, and file contents
+# are stored in a list.
 def main():
     files = [f for f in os.listdir(img_path)]
     for f in files:
